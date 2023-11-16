95be47a6c42f63f6282857a4206aab0516f9013a
MuhamadAjiW <16521119@mahasiswa.itb.ac.id>
2023-11-17 01:18:28 +0700

    feat: integrated auth to bookp and playlist

--- src/routes/bookp-route.ts
+++ src/routes/bookp-route.ts
@@ -1,27 +1,37 @@
 import { Router } from "express";
 import { BookPController } from "../controllers/bookp-controller";
+import { AuthMiddleware } from "../middlewares/auth-middleware";
+import { AuthTypes } from "../types/enums/AuthTypes";
 
 export class BookPRoute {
     bookPController: BookPController;
+    authMiddleware: AuthMiddleware;
 
     constructor() {
         this.bookPController = new BookPController();
+        this.authMiddleware = new AuthMiddleware();
     }
 
     getRoutes() {
         return Router()
             .get('/books',
+                this.authMiddleware.authenticate(AuthTypes.ANYAUTH),
                 this.bookPController.getBooks())
             .post('/books',
+                this.authMiddleware.authenticate(AuthTypes.ANYAUTH),
                 this.bookPController.createBookP())
             .get('/books/:identifier',
+                this.authMiddleware.authenticate(AuthTypes.ANYAUTH),
                 this.bookPController.getBookPByID())
             .delete('/books/:identifier',
+                this.authMiddleware.authenticate(AuthTypes.ANYAUTH),
                 this.bookPController.deleteOneBookP())
             .patch('/books/:identifier',
+                this.authMiddleware.authenticate(AuthTypes.ANYAUTH),
                 this.bookPController.updateOneBook())
 
             .get('/authors/:identifier/books',
+                this.authMiddleware.authenticate(AuthTypes.ANYAUTH),
                 this.bookPController.getBookPByAuthor())
     }
 }
--- src/routes/playlist-route.ts
+++ src/routes/playlist-route.ts
@@ -1,29 +1,40 @@
 import { Router } from "express";
 import { PlaylistController } from "../controllers/playlist-controller";
+import { AuthMiddleware } from "../middlewares/auth-middleware";
+import { AuthTypes } from "../types/enums/AuthTypes";
 
 export class PlaylistRoute {
     playlistController: PlaylistController;
+    authMiddleware: AuthMiddleware;
 
     constructor() {
         this.playlistController = new PlaylistController();
+        this.authMiddleware = new AuthMiddleware();
     }
 
     getRoutes() {
         return Router()
             .post('/playlists',
+                this.authMiddleware.authenticate(AuthTypes.ANYAUTH),
                 this.playlistController.createPlaylist())
             .delete('/playlists/:identifier',
+                this.authMiddleware.authenticate(AuthTypes.ANYAUTH),
                 this.playlistController.deleteOnePlaylist())
             .patch('/playlists/:identifier',
+                this.authMiddleware.authenticate(AuthTypes.ANYAUTH),
                 this.playlistController.updateOnePlaylist())
             .get('/playlists/:identifier/books',
+                this.authMiddleware.authenticate(AuthTypes.ANYAUTH),
                 this.playlistController.getPlaylistBooks())
             .post('/playlists/:identifier/books',
+                this.authMiddleware.authenticate(AuthTypes.ANYAUTH),
                 this.playlistController.addPlaylistBook())
             .delete('/playlists/:identifier/books',
+                this.authMiddleware.authenticate(AuthTypes.ANYAUTH),
                 this.playlistController.deletePlaylistBook())
 
             .get('/authors/:identifier/playlists',
+                this.authMiddleware.authenticate(AuthTypes.ANYAUTH),
                 this.playlistController.getPlaylists())
     }
 }
